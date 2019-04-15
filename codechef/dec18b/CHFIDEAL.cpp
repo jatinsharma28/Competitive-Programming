46de59b5bc7656750aa45a8c633e6b4505ddfdca
jatin sharma <32265911+jatinsharma28@users.noreply.github.com>
2019-04-15 21:41:52 +0530

    Create CHFIDEAL.cpp

---
+++ codechef/dec18b/CHFIDEAL.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int y;
+    cout<<"3"<<endl;
+    fflush(stdout);
+    cin>>y;
+    if(y==1)
+    {
+        cout<<"2"<<endl;
+        fflush(stdout);
+    }
+    else
+    {
+        cout<<"1"<<endl;
+        fflush(stdout);
+    }
+
+    return 0;
+}
