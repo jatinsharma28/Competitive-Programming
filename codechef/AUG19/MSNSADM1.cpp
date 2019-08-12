fa75bfe7aad3c01b14aeb0bce38d8641328600bb
jatin sharma <32265911+jatinsharma28@users.noreply.github.com>
2019-08-12 15:41:12 +0530

    Update MSNSADM1.cpp

--- codechef/AUG19/MSNSADM1.cpp
+++ codechef/AUG19/MSNSADM1.cpp
@@ -1,1 +1,44 @@
+#problem link=https://www.codechef.com/AUG19B/problems/MSNSADM1
 
+
+
+#include<bits/stdc++.h>
+#define ll long long int
+ll m=1e9+7;
+using namespace std;
+
+int main()
+{
+   int t;
+   cin>>t;
+   while(t!=0)
+   {
+       int n;
+       cin>>n;
+       int a[n];
+       int b[n];
+       int ma=0,d=0;
+       for(int i=0;i<n;i++)
+       {
+           cin>>a[i];
+       }
+       for(int i=0;i<n;i++)
+       {
+           cin>>b[i];
+       }
+       for(int j=0;j<n;j++)
+       {
+           d= (a[j]*20) - (b[j]*10);
+           if(d>ma)
+           {
+               ma=d;
+           }
+       }
+       cout<<ma<<endl;
+
+t--;
+
+   }
+
+    return 0;
+}
