6276bcd389c33ffa7782d09e320953465335e03b
jatin sharma <32265911+jatinsharma28@users.noreply.github.com>
2019-08-12 15:43:06 +0530

    Create DSTAPLS.cpp

---
+++ codechef/AUG19/DSTAPLS.cpp
@@ -0,0 +1,32 @@
+//problem link=https://www.codechef.com/AUG19B/problems/DSTAPLS/
+
+#include<bits/stdc++.h>
+#define ll long long int
+ll m=1e9+7;
+using namespace std;
+
+int main()
+{
+   int t;
+   cin>>t;
+   while(t!=0)
+   {  ll n=0,k=0;
+   cin>>n>>k;
+   ll d=n/k;
+   if(d%k==0)
+   {
+       cout<<"NO"<<endl;
+   }
+   else
+   {
+       cout<<"YES"<<endl;
+   }
+
+
+t--;
+
+   }
+
+    return 0;
+}
+
