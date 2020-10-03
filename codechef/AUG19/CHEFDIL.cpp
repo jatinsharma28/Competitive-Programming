822c28d742b4fab583db317d7ad0c5e09887f3e6
Vikramaditya Singh Kachhwaha <37743343+Vsk1997@users.noreply.github.com>
2020-10-03 12:53:52 +0530

    Update CHEFDIL.cpp

--- codechef/AUG19/CHEFDIL.cpp
+++ codechef/AUG19/CHEFDIL.cpp
@@ -3,6 +3,9 @@
 
 #include<bits/stdc++.h>
 #define ll long long int
+#define max3(a,b,c) max(a,max(b,c))
+#define min3(a,b,c) min(a,min(b,c))
+#define forn(n) for(int i=0;i<n;i++)
 ll m=1e9+7;
 using namespace std;
 
